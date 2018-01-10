8d62b58c02173c757d1538973f1038ae4f0424bd
Alon Albert <alon.albert@gmail.com>
2018-01-10 08:12:13 +0200

    Add script for deluge pushover integration

---
+++ deluge-pushover.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#
+
+import requests
+import configparser
+import sys
+import os
+
+if __name__ == '__main__':
+  # dir = sys.argv[3]
+  name = sys.argv[2]
+  config = configparser.ConfigParser()
+  config.read(os.path.expanduser('~/.pushover'))
+  setup = config["setup"]
+  r = requests.post('https://api.pushover.net/1/messages.json',
+                    data={
+                      'user': setup['user'],
+                      'token': setup['deluge-token'],
+                      'sound': 'magic',
+                      'message': name
+                    })
+
+  print r.status_code
