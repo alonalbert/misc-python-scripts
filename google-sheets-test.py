740ddd44bd0bf8480cbeb7597ebe3fcdb299c2a9
Alon Albert <alon.albert@gmail.com>
2019-03-06 19:20:36 +0200

    Misc

--- google-sheets-test.py
+++ google-sheets-test.py
@@ -1,14 +1,15 @@
-import sys
-
-import httplib2
 import os
+import sys
+import datetime
 
 from apiclient import discovery
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SECRET_FILE = os.path.expanduser('~/.duolingo-tracker-client.json')
-RANGE_NAME = 'Log!A1:D4'
+DAY_ZERO = datetime.datetime(1899, 12, 30)
+
+RANGE = 'Log!A1:D1'
 
 if __name__ == '__main__':
     credentials = service_account.Credentials.from_service_account_file(SECRET_FILE, scopes=SCOPES)
@@ -16,8 +17,22 @@ if __name__ == '__main__':
 
     sheet = service.spreadsheets()
 
-    spreadsheetId = sys.argv[1]
+    spreadsheet_id = sys.argv[1]
 
-    result = sheet.values().get(spreadsheetId=spreadsheetId, range=RANGE_NAME).execute()
-    values = result.get('values', [])
-    print(values)
+    valuesApi = service.spreadsheets().values()
+
+    request = valuesApi.get(spreadsheetId=spreadsheet_id, range='Log!A:D')
+    response = request.execute()
+    values = response['values']
+
+    date = datetime.datetime.now() - DAY_ZERO
+    print(date)
+    values.insert(1, [date.days, 11, 12, 13])
+
+    body = {
+        'values': values
+    }
+
+    request = valuesApi.update(spreadsheetId=spreadsheet_id, range='Log!A:D',
+                               valueInputOption='USER_ENTERED', body=body)
+    request.execute()
