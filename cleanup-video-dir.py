13e6166223fb71d060cebb2ddef598115bb4cfe3
Alon Albert <aalbert@google.com>
2021-11-03 15:20:35 -0700

    Remove with rmtree Remove only dir updated more than 14 days ago

--- cleanup-video-dir.py
+++ cleanup-video-dir.py
@@ -2,8 +2,7 @@
 
 import os
 import shutil
-import tempfile
-import re
+import time
 from sys import argv
 
 MIN_FILE_SIZE = 120 * 1024 * 1024
@@ -21,37 +20,35 @@ def countLargeFiles(dir):
       count += countLargeFiles(path)
   return count
 
-def cleanup(root, recycleBin):
+def cleanup(root):
   files = os.listdir(root)
   for file in files:
     path = os.path.join(root, file)
     if os.path.isdir(path):
       count = countLargeFiles(path)
       if count == 0:
-        print("Removing %s" % path)
-        dest = tempfile.mkdtemp("", file, recycleBin)
-        shutil.move(path, dest)
+        sinceUpdated = (time.time() - os.path.getmtime(path)) / 60 / 60 / 24
+        if sinceUpdated > 14:
+          print("Removing %s" % path)
+          shutil.rmtree(path)
+        # dest = tempfile.mkdtemp("", file, recycleBin)
+        # shutil.move(path, dest)
 
 
 if __name__ == '__main__':
-  recycleBin = argv[-1]
-  if os.path.exists(recycleBin):
-    shutil.rmtree(recycleBin)
-  os.makedirs(recycleBin)
+  # for root in argv[1:]:
+  #   files1 = os.listdir(root)
+  #   for file1 in files1:
+  #     path1 = os.path.join(root, file1)
+  #     if os.path.isdir(path1):
+  #       files2 = os.listdir(path1)
+  #       for file2 in files2:
+  #         if re.match("S\\d+", file2):
+  #           path2 = os.path.join(path1, file2)
+  #           cleanup(path2)
 
-  for root in argv[1:-1]:
-    files1 = os.listdir(root)
-    for file1 in files1:
-      path1 = os.path.join(root, file1)
-      if os.path.isdir(path1):
-        files2 = os.listdir(path1)
-        for file2 in files2:
-          if re.match("S\\d+", file2):
-            path2 = os.path.join(path1, file2)
-            cleanup(path2, recycleBin)
-
-  for root in argv[1:-1]:
-    cleanup(root, recycleBin)
+  for root in argv[1:]:
+    cleanup(root)
 
 
 
